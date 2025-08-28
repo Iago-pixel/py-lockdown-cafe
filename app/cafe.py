9157be3bbdd13a936f8e2a7501d08573ef96493e
Iago Vinícius Souza de Sales <pristonvinicius@hotmail.com>
2025-08-28 02:21:49 -0300

    'Solution'

--- app/cafe.py
+++ app/cafe.py
@@ -8,10 +8,26 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
+        base_text_error = (
+            f"Visitor {visitor.get('name', 'unknown')}"
+            " cannot enter {self.name}"
+        )
+        expiration_date = visitor['vaccine']['expiration_date']
+
         if "vaccine" not in visitor:
-            raise NotVaccinatedError(f"Visitor {visitor.get('name', 'unknown')} cannot enter {self.name}: not vaccinated.")
+            raise NotVaccinatedError(f"{base_text_error}: not vaccinated.")
         elif date.today() > visitor["vaccine"]["expiration_date"]:
-            raise OutdatedVaccineError(f"Visitor {visitor.get('name', 'unknown')} cannot enter {self.name}: expired vaccine {visitor['vaccine']['expiration_date'].strftime('%Y-%m-%d')}")
+            raise OutdatedVaccineError(
+                (
+                    f"{base_text_error}: expired vaccine"
+                    f" {expiration_date.strftime('%Y-%m-%d')}"
+                )
+            )
         elif not visitor.get("wearing_a_mask"):
-            raise NotWearingMaskError(f"Visitor {visitor.get('name', 'unknown')} cannot enter {self.name}: need to wear a mask")
+            raise NotWearingMaskError(
+                (
+                    f"{base_text_error}: "
+                    "need to wear a mask"
+                )
+            )
         return f"Welcome to {self.name}"
